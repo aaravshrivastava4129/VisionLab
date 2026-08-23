da7dd519f1e8649d0958ea3900dd197b85889edf
Aarav Shrivastava <aaravshrivastava4129@gmail.com>
2026-08-23 16:00:34 +0530

    Starting v1.0.0

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import cv2
+
+from PySide6.QtCore import QTimer, Qt
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QComboBox
+)
+
