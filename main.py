426abf24cca3fb089b78641d9fcd15d22513ee13
Aarav Shrivastava <aaravshrivastava4129@gmail.com>
2026-08-26 18:53:29 +0530

    v1.1.0 Prevent Restore Down

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import cv2
 
-from PySide6.QtCore import QTimer, Qt
-from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtCore import QTimer, Qt, QEvent, QThread
+from PySide6.QtGui import QImage, QPixmap, QPainter, QPainterPath
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -11,28 +11,40 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QWidget,
-    QComboBox
+    QComboBox,
+    QMessageBox
 )
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
         self.camera = cv2.VideoCapture(0)
         self.timer = QTimer()
         self.timer.timeout.connect(self.updateFrame)
 
         self.setWindowTitle("VisionLab")
-        self.resize(900, 600)
+        self.resize(1920, 1040)
 
         self.cameraLabel = QLabel("Camera is off")
         self.cameraLabel.setObjectName("cameraLabel")
         self.cameraLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.cameraLabel.setContentsMargins(0, 0, 0, 0)
         self.cameraLabel.setScaledContents(False)
 
+        self.frame_holder = QWidget()
+        self.frame_holder.setObjectName("frame_holder")
+
+
         self.headingLabel = QLabel("VisionLab")
+        self.headingLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.headingLabel.setObjectName("headingLabel")
 
+        self.compVisionLabel = QLabel("Computer Vision")
+        self.compVisionLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.compVisionLabel.setObjectName("compVisionLabel")
+        
         self.modeBox = QComboBox()
         self.modeBox.setObjectName("modeBox")
         self.modeBox.addItem("Normal")
@@ -55,99 +67,139 @@ class MainWindow(QMainWindow):
         main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.setSpacing(0)
 
-        main_layout.addWidget(self.cameraLabel, stretch=1)
-        main_layout.addWidget(self.side_panel)
+        
+        main_layout.addWidget(self.frame_holder, stretch=68)
+        main_layout.addWidget(self.side_panel, stretch=32)
 
+        frame_layout = QVBoxLayout(self.frame_holder)
+
+        frame_layout.addWidget(self.cameraLabel, stretch=1)
+
+        
         side_layout = QVBoxLayout(self.side_panel)
-        side_layout.setContentsMargins(20, 30, 20, 30)
-        side_layout.setSpacing(15)
+        side_layout.setContentsMargins(20, 15, 20, 30)
+        side_layout.setSpacing(0)
 
         side_layout.addWidget(self.headingLabel)
+        side_layout.addWidget(self.compVisionLabel)
         side_layout.addWidget(self.modeBox)
+        side_layout.addStretch(1)
         side_layout.addWidget(self.startBtn)
         side_layout.addWidget(self.stopBtn)
-
-        side_layout.addStretch(1)
+        
 
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
-        def start_camera(self):
+        self._is_ready = False
 
-            self.cameraLabel.setText("Camera is starting...")
-            self.cameraLabel.setStyleSheet("color: #FF3366; font-weight: bold;")
-            self.startBtn.hide()
-            self.stopBtn.show()
+    def start_camera(self):
 
-            QApplication.processEvents()
+        self.cameraLabel.setText("Camera is starting...")
+        self.cameraLabel.setStyleSheet("color: #FF3366; font-weight: bold;")
+        self.startBtn.hide()
+        self.stopBtn.show()
 
-            if not self.camera.isOpened():
-                self.camera.open(0)
+        QApplication.processEvents()
 
-            self.timer.start(1) #Gives Frames 33 is for 30FPS
+        if not self.camera.isOpened():
+            self.camera.open(0)
 
-        def stop_camera(self):
-            self.timer.stop()
-            self.camera.release()
-            self.cameraLabel.setText("Camera is off")
-            self.cameraLabel.setStyleSheet("color: #FF3366; font-weight: bold;")
-            self.startBtn.show()
-            self.stopBtn.hide()
+        self.timer.start(1) #CHANGE TO 33 BEFORE FINALLY MAKING IT PUBLIC
 
-        def update_frame(self):
+    def stop_camera(self):
+        self.timer.stop()
+        self.camera.release()
+        self.cameraLabel.setText("Camera is off")
+        self.cameraLabel.setStyleSheet("color: #FF3366; font-weight: bold;")
+        self.startBtn.show()
+        self.stopBtn.hide()
+    
+    def updateFrame(self):
 
-            mode = self.modeBox.currentText()
+        mode = self.modeBox.currentText()
 
-            success, frame = self.camera.read()
+        success, frame = self.camera.read()
+        
+        if not success:
+            return
 
-            if not success:
-                return
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        if mode == "Normal":
+            pass
+        elif mode == "Grayscale":
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        elif mode == "Edges":
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            frame = cv2.Canny(frame, 100, 200)
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
-            frame = cv2.flip(frame, 1)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.display_image(frame)
 
-            if mode == "Normal":
-                pass
-            elif mode == "Grayscale":
-                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-            elif mode == "Edges":
-                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                frame = cv2.Canny(frame, 100, 200)
-                frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+    def display_image(self, frame):
+        height, width, channels = frame.shape
+        bytes_per_line = channels * width
 
-            self.display_image(frame)
+        image = QImage(
+            frame.data,
+            width,
+            height,
+            bytes_per_line,
+            QImage.Format_RGB888
+        )
 
-        def display_image(self, frame):
-            height, width, channels = frame.shape
-            bytes_per_line = channels * width
+        pixmap = QPixmap.fromImage(image)
 
-            image = QImage(
-                frame.data,
-                width,
-                height,
-                bytes_per_line,
-                QImage.Formate_RGB888
-            )
+        scaled_pixmap = pixmap.scaled(
+            self.cameraLabel.size(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation,
+            Qt.AlignmentFlag.AlignCenter
+        )
 
-            pixmap = QPixmap.fromImage(image)
+        rounded_pixmap = QPixmap(scaled_pixmap.size())
+        rounded_pixmap.fill(Qt.GlobalColor.transparent)
 
-            scaled_pixmap = pixmap.scaled(
-                self.cameraLabel.size(),
-                Qt.AspectRatioMode.KeepAspectRatio,
-                Qt.TransformationMode.SmoothTransformation,
-                Qt.AlignmentFlag.AlignCenter
-            )
+        painter = QPainter(rounded_pixmap)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-            self.cameraLabel.setPixmap(scaled_pixmap)
-            self.cameraLabel.setContentsMarigns(0, 0, 0, 0)
+        path = QPainterPath()
+        path.addRoundedRect(0, 0, scaled_pixmap.width(), scaled_pixmap.height(), 10, 10)
+        
+        painter.setClipPath(path)
+        painter.drawPixmap(0, 0, scaled_pixmap)
+        painter.end()
 
-        def closeEvent(self, event):
-            self.timer.stop()
-            self.camera.release()
-            event.accept()
+        self.cameraLabel.setPixmap(rounded_pixmap)
+        self.cameraLabel.setContentsMargins(0, 0, 0, 0)
 
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self._is_ready = True
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.WindowStateChange:
+            if hasattr(self, '_is_ready') and self._is_ready:
+                if not self.isMinimized() or self.isMaximized():
+                    self._is_ready = False
+                    QMessageBox.warning(self, "Notice", "Sorry, this programme doesn't support changing the layout size.")
+                    self.showMaximized()
+                    event.accept()
+                    self._is_ready = True
+                    return
+        super().changeEvent(event)
+        
+
+    def closeEvent(self, event):
+        self.timer.stop()
+        self.camera.release()
+        event.accept()
+        
 with open("style.qss", "r") as file:
     stylesheet = file.read()
 
@@ -155,4 +207,6 @@ app = QApplication(sys.argv)
 app.setStyleSheet(stylesheet)
 
 window = MainWindow()
-window.show()
+window.showMaximized()
+
+sys.exit(app.exec())
